a73a27a8efe2b041943d8e2fded815cb351a7e6f
Zilliz Nocilicon <yuduc2@illinois.edu>
2025-04-11 04:08:23 +0800

    added some log in client, the join request fired from clients never returns, gonna add logs to server to explore why

--- src/shardmaster/client.go
+++ src/shardmaster/client.go
@@ -9,12 +9,18 @@ import "time"
 import "crypto/rand"
 import "math/big"
 
+import "sync"
+
+import "log"
+
 type Clerk struct {
 	servers []*labrpc.ClientEnd
 	// Your data here.
 
 	Client_Serial_Number int64 // serial number with which we uniquely identify the client
 	Sequence_Number int //sequence number for requests used for duplication detection to ensure linearizability
+
+	mu      sync.Mutex
 }
 
 func nrand() int64 {
@@ -39,15 +45,18 @@ func (ck *Clerk) Query(num int) Config {
 	// Your code here.
 	args.Num = num
 
+
 	args.Client_Serial_Number = ck.Client_Serial_Number
 	ck.Sequence_Number = ck.Sequence_Number + 1
 	args.Sequence_Number = ck.Sequence_Number
+	log.Printf("client %d init Query request with num %d, sequence number %d", ck.Client_Serial_Number, num, args.Sequence_Number)
 	for {
 		// try each known server.
 		for _, srv := range ck.servers {
 			var reply QueryReply
 			ok := srv.Call("ShardMaster.Query", args, &reply)
 			if ok && reply.WrongLeader == false {
+				log.Printf("client %d received Query request with num %d, sequence number %d", ck.Client_Serial_Number, num, args.Sequence_Number)
 				return reply.Config
 			}
 		}
@@ -60,15 +69,19 @@ func (ck *Clerk) Join(servers map[int][]string) {
 	// Your code here.
 	args.Servers = servers
 
+
 	args.Client_Serial_Number = ck.Client_Serial_Number
 	ck.Sequence_Number = ck.Sequence_Number + 1
 	args.Sequence_Number = ck.Sequence_Number
+	log.Printf("client %d init Join request sequence number %d", ck.Client_Serial_Number, args.Sequence_Number)
+
 	for {
 		// try each known server.
 		for _, srv := range ck.servers {
 			var reply JoinReply
 			ok := srv.Call("ShardMaster.Join", args, &reply)
 			if ok && reply.WrongLeader == false {
+				log.Printf("client %d received Join request sequence number %d", ck.Client_Serial_Number, args.Sequence_Number)
 				return
 			}
 		}
@@ -81,15 +94,19 @@ func (ck *Clerk) Leave(gids []int) {
 	// Your code here.
 	args.GIDs = gids
 
+
 	args.Client_Serial_Number = ck.Client_Serial_Number
 	ck.Sequence_Number = ck.Sequence_Number + 1
 	args.Sequence_Number = ck.Sequence_Number
+	log.Printf("client %d init Leave request sequence number %d", ck.Client_Serial_Number, args.Sequence_Number)
+
 	for {
 		// try each known server.
 		for _, srv := range ck.servers {
 			var reply LeaveReply
 			ok := srv.Call("ShardMaster.Leave", args, &reply)
 			if ok && reply.WrongLeader == false {
+				log.Printf("client %d received Leave request sequence number %d", ck.Client_Serial_Number, args.Sequence_Number)
 				return
 			}
 		}
@@ -103,15 +120,18 @@ func (ck *Clerk) Move(shard int, gid int) {
 	args.Shard = shard
 	args.GID = gid
 
+
 	args.Client_Serial_Number = ck.Client_Serial_Number
 	ck.Sequence_Number = ck.Sequence_Number + 1
 	args.Sequence_Number = ck.Sequence_Number
+	log.Printf("client %d init Move request with shard %d, gid %d, and sequence number %d", ck.Client_Serial_Number, shard, gid, args.Sequence_Number)
 	for {
 		// try each known server.
 		for _, srv := range ck.servers {
 			var reply MoveReply
 			ok := srv.Call("ShardMaster.Move", args, &reply)
 			if ok && reply.WrongLeader == false {
+				log.Printf("client %d received Move request with shard %d, gid %d, and sequence number %d", ck.Client_Serial_Number, shard, gid, args.Sequence_Number)
 				return
 			}
 		}
